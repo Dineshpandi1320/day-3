81d7b9f5851561f0075acc41a1db57bc3567992e
Dineshpandi1320 <118879743+Dineshpandi1320@users.noreply.github.com>
2022-11-26 16:42:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
-//JASON Javascript 
-var obj =[ 
-{
-    "name": "Dinesh",
-    "age":"20",
-    "gender":"male",
-    "subject": ["tamil","english","maths","science","social"]
-}
-]
-for(var i=0; i<obj.length; i++)
-{
-    //console.log(obj);
-}
-//FOR IN LOOP
+var obj1 ={name:"person1",age:5};
+var  obj2={age:5,name:"person1"};
+JSON.stringify(obj1) === JSON.stringify(obj2);
+var flag=true;
 
-for(var i in obj)
-{
-    //console.log(obj)
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
 }
-//FOR OF LOOP
-
-
-for(var  i of obj)
-{
-   // console.log(obj)
+else {
+    flag=false;
 }
-obj.forEach(element => {
-    //console.log(element)
-});
+console.log("is object equal"+flag);
 
 //output
+//is object equaltrue
 
-// {name: 'Dinesh', age: '20', gender: 'male', subject: Array(5)}
+// country name, region , sub region,population
 
-// : 
-// {name: 'Dinesh', age: '20', gender: 'male', subject: Array(5)}
-
-// : 
-// {name: 'Dinesh', age: '20', gender: 'male', subject: Array(5)}
-
-// : 
-// Object
-// age
-// : 
-// "20"
-// gender
-// : 
-// "male"
-// name
-// : 
-// "Dinesh"
-// subject
-// : 
-// (5) ['tamil', 'english', 'maths', 'science', 'social']
-
-
-
-
-//resume create JSON format
-var resume={
-    "Name" :"Dineshpandi",
-    "Age"  :"20",
-    "Gender" :"male",
-    "qualification" :["10th","12th","U.G"],
-    "location" :"Dindigul",
-    "languages_know" :["tamil","english"],
-    "programming_skils" :["c","java","python","javascript"],
-    "exprience" :"fresher"
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send()
+request.onload=function()
+{
+    var data = request.response
+    var result=JSON.parse(data);
+    for(var obj of result)
+    {
+        console.log(obj.name,obj.region,obj.subregion,obj.population)
+    }
 }
-console.log(resume)
 
-// //output
-// {Name: 'Dineshpandi', Age: '20', Gender: 'male', qualification: Array(3), location: 'Dindigul', …}
+
+//country flags
+
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send()
+request.onload=function()
+{
+    var data = request.response
+    var result=JSON.parse(data);
+    for(var obj of result)
+    {
+        console.log(obj.flags.png)
+    }
+}
